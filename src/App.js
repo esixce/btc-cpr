b6062b84c0b5415317d21b7416b54e3386d1a07e
esixce <98708432+esixce@users.noreply.github.com>
2023-01-04 09:18:45 -0500

    new links

--- src/App.js
+++ src/App.js
@@ -4,20 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ENGLISH</h1>
+      <h2>seminars</h2>
+      <p>Des femmes mentorship</p>
+      <p>Summer of Bitcoin</p>
+      <p>Chaincode labs</p>
+
+      <h1>articles</h1>
+      <p></p>
+
+      <h1>guides</h1>
+      <p>odells how to https://werunbtc.com/</p>
+      
+
+      <h1>developers</h1>
+      <p></p>
+
+      <h1>onboarding</h1>
+      <p></p>
+
+      <h1>ART</h1>
+      <h1>cool stuff</h1>
+      <p>pixel art</p>
+
+
+      <p>TECH</p>
+      <p>Download Bitcoin and hash check</p>
+      <p>https://bitcoincore.org/en/download/</p>
+      <p>test coverage</p>
+      <p>https://marcofalke.github.io/btc_cov/total.coverage/index.html</p>
+
+
+      <h1>SPANISH</h1>
+
     </div>
   );
 }
